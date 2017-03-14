532ad75b026bea0aa011b04fc283916d7703711d
Dipu Rajak <drajak@gannett.com>
2017-03-14 14:29:41 -0400

    initial check in

---
+++ TagView/TagView.h
@@ -0,0 +1,19 @@
+//
+//  TagView.h
+//  TagView
+//
+//  Created by Rajak, Dipu on 3/14/17.
+//  Copyright © 2017 Rajak, Dipu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TagView.
+FOUNDATION_EXPORT double TagViewVersionNumber;
+
+//! Project version string for TagView.
+FOUNDATION_EXPORT const unsigned char TagViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TagView/PublicHeader.h>
+
+
